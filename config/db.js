1ed5893b8653393cda4b3bae45952948270062f6
Adria Vieira <adriavieira731@gmail.com>
2020-09-22 20:06:12 -0400

    initial commit

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production') {
-    module.exports ={ mongoURI: 'mongo "mongodb+srv://nodejsnoyoutube.qvzmb.mongodb.net/blogapp" --username AdriaVieira'  }
+    module.exports ={ mongoURI: 'mongodb+srv://AdriaVieira:<123456>@nodejsnoyoutube.qvzmb.mongodb.net/blogapp?retryWrites=true&w=majority'  }
 } else {
     module.exports ={ mongoURI: 'mongodb://localhost/blogapp' }
 }
